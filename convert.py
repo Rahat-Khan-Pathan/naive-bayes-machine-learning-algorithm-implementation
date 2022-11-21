d05caf670fee980f94ec0e42e7ddaa7e8fad8294
Rahat <rahatkhanpathan99@gmail.com>
2022-11-21 20:02:16 +0600

    updated main.py

--- convert.py
+++ convert.py
@@ -1,12 +1,16 @@
 import csv
 import pandas as pd
+import time
 csv_file = "data.csv"
 data = {
   "Outlook": ["Rainy", "Rainy", "Overcast","Sunny","Sunny","Sunny","Overcast","Rainy","Rainy","Sunny","Rainy","Overcast","Overcast","Sunny"],
   "Temp": ["Hot","Hot","Hot","Mild","Cool","Cool","Cool","Mild","Cool","Mild","Mild","Mild","Hot","Mild"],
   "Humidity": ["High", "High", "High","High","Normal","Normal","Normal","High","Normal","Normal","Normal","High","Normal","High"],
-  "Windy":['n','y','n','n','n','y','y','n','n','n','y','y','n','y'],
+  "Windy":['no','yes','no','no','no','yes','yes','no','no','no','yes','yes','no','yes'],
   "Play":["no","no","yes","yes","yes","no","yes","no","yes","yes","yes","yes","yes","no"]
 }
+print("Converting dataset to csv format...\n")
+time.sleep(1)
 df = pd.DataFrame.from_dict(data, orient="index")
 df.to_csv("data.csv")
+print("Convertion done. Now you can train your model\n")
--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+import time
 
 # weather dataset 
 
 def read_data():
+    print("Reading data from csv...\n")
+    time.sleep(2)
     df = pd.read_csv("data.csv", index_col=0)
     d = df.to_dict("split")
     d = dict(zip(d["index"], d["data"]))
+    print("Done...\n")
     return d
 class NaiveBayes:
     def __init__(self,x,y,df) -> None:
@@ -22,11 +26,16 @@ class NaiveBayes:
         self.totalNo=0
     @staticmethod
     def pre_processing(df):
+        print("Processing your data...\n")
+        time.sleep(2)
         x = df.drop(df.columns[-1],axis=1)
         y = df[df.columns[-1]]
+        print("Data processed!\n")
         return x, y
     
     def train_model(self):
+        print("Training your model...\n")
+        time.sleep(2)
         for item in self.df[self.y.name]:
             if item=="yes":
                 self.totalYes+=1
@@ -43,8 +52,11 @@ class NaiveBayes:
                     elif self.df[feature][indx]==uniqueItem:
                         no+=1
                 self.dct[uniqueItem]={"yes":yes,"no":no,"total":yes+no}
+        print("Model trained. You can now test!\n")
     
     def test_model(self,testList):
+        print("Testing...\n")
+        time.sleep(2)
         sum1Yes=(self.totalYes/self.total)
         sum2Yes=1
         for testItem in testList:
@@ -76,13 +88,14 @@ for uniqueCol in list(x.columns):
     if x<0 or x>=len(columnData):
         print("Error occured. Try again!")
     test.append(columnData[x])
-# print(test)
+    print("\n\n")
+print(f"\nYour test data is: {test}\n")
 sumYes,sumNo=naive.test_model(test)
-print(f"Probability of yes: {sumYes}\nProbability of no: {sumNo}")
+print(f"Probability of yes: {sumYes}\nProbability of no: {sumNo}",end="\n\n")
 if sumYes > sumNo:
-    print("answer is yes")
+    print(f"answer is yes, you can {y.name}")
 else:
-    print("answer is no")
+    print(f"answer is no, you can't {y.name}")
 
             
 
